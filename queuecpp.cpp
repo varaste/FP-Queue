3161b8c1f47874393f9f2539d2a501119da96f8c
Arya Varaste <30382009+varaste@users.noreply.github.com>
2018-07-16 09:09:38 +0430

    Update queuecpp.cpp

--- queuecpp.cpp
+++ queuecpp.cpp
@@ -1,34 +1,19 @@
 #include<iostream>
 #include<vector>
 #include<string.h>
-#include <windows.h>
-//12
 using namespace std;
 _OUTPUT_HANDLE); //Color for text
 HANDLE hstdin = GetStdHandle(STD_INPUT_HANDLE); //Color for text
-
-	cout << "for peek  ------- 3 \n";
-	SetConsoleTextAttribute(hstdout, 0xb);
-	cout << "for isfull  ----- 4\
 
 void peek()
 		SetConsoleTextAttribute(hstdout, 0x0f);
 		cout << "Empty array\n";
 
 		cout << "Empty array\n";
-	}//end else
-
-}//end pop
-
-
 
 
 			access();			//namayesh nenu
 			pop();
-			break;
-		case 3:
-			access();			//namayesh nenu
-			peek();
 			cout << endl;
 			break;
 		case 4:
